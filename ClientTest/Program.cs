bb027e4bc84e969393512060497ab2193363bd17
Simon Heiss <simonheiss87@gmail.com>
2018-09-16 19:39:40 +0200

    with pw auth done

--- ClientTest/Program.cs
+++ ClientTest/Program.cs
@@ -10,8 +10,31 @@ namespace ClientTest
     {
         static void Main(string[] args)
         {
-            Authenticate().Wait();
+            //Authenticate().Wait();
+            AuthenticateWithPw().Wait();
             Console.ReadKey();
+        }
+
+        private static async Task AuthenticateWithPw()
+        {
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "ro.client", "secret");
+            var tokenResponse = await tokenClient.RequestResourceOwnerPasswordAsync("alice", "password", "api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
         }
 
         private static async Task Authenticate()
